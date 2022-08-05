951d3200146ec23f91ae62c08c69bf91a88223ae
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2022-08-05 21:49:56 +0100

    update

--- src/redux/apiCalls.ts
+++ src/redux/apiCalls.ts
@@ -7,7 +7,7 @@ export const search = async (dispatch: any, searchTerm: any) => {
     dispatch(searchStart());
     try{
         const res = await axios.get(`http://localhost:8000/${searchTerm}`)
-        dispatch(searchSuccess(res.data))
+        dispatch(searchSuccess(res.data.results))
         console.log(res)
     }catch(err: any){
         dispatch(searchFailure(err.response))
